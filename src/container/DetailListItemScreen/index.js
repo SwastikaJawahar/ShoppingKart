0413dda8577062b28cfe42110b0dc03166796c0a
SwastikaJawahar <136087727+SwastikaJawahar@users.noreply.github.com>
2023-10-22 11:18:50 +0100

    Classcomponent

--- src/container/DetailListItemScreen/index.js
+++ src/container/DetailListItemScreen/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 
-const DetailListItemScreen = props => {
-  const {route} = props;
-  return (
-    <View style={styles.container}>
-      <Text style={styles.texttitle}>{route.params.title}</Text>
-      <Text style={styles.textdesc}>{route.params.desc}</Text>
-    </View>
-  );
-};
+class DetailListItemScreen extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    const {route} = this.props;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.texttitle}>{route.params.title}</Text>
+        <Text style={styles.textdesc}>{route.params.desc}</Text>
+      </View>
+    );
+  }
+}
 
 export default DetailListItemScreen;
